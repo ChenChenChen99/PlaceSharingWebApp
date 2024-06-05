95f6b35cadf700e327138539e75b1f7fa6997f70
QianChen <clairechenq0909@gmail.com>
2024-06-05 12:31:04 -0700

    update auth

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,5 @@
-const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 
@@ -67,7 +68,7 @@ const createPlace = async (req, res, next) => {
     );
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -81,14 +82,13 @@ const createPlace = async (req, res, next) => {
     description,
     address,
     location: coordinates,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/400px-Empire_State_Building_%28aerial_view%29.jpg',
-    creator
+    image: req.file.path,
+    creator: req.userData.userId
   });
   
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     const error = new HttpError('Creating place failed, please try again', 500);
     return next(error);
@@ -98,6 +98,8 @@ const createPlace = async (req, res, next) => {
     const error = new HttpError('Could not find user for provided id', 404);
     return next(error);
   }
+
+  console.log(user);
 
   try {
     const sess = await mongoose.startSession();
@@ -139,6 +141,11 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError('You are not allowed to edit this place.', 401);
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -173,23 +180,35 @@ const deletePlace = async (req, res, next) => {
       const error = new HttpError('Could not find place for this id.', 404);
       return next(error);
     }
+
+    if (place.creator.id !== req.userData.userId) {
+      const error = new HttpError(
+        'You are not allowed to delete this place.',
+        401
+      );
+      return next(error);
+    }
+
+    const imagePath = place.image;
   
     try {
       const sess = await mongoose.startSession();
       sess.startTransaction();
-    //   await place.remove({ session: sess });
       await Place.deleteOne({ _id: place._id }, { session: sess });
       place.creator.places.pull(place); 
       await place.creator.save({ session: sess });
       await sess.commitTransaction();
     } catch (err) {
-        console.log(err);
       const error = new HttpError(
         'Something went wrong, could not delete place.',
         500
       );
       return next(error);
     }
+
+    fs.unlink(imagePath, err => {
+      console.log(err);
+    });
   
     res.status(200).json({ message: 'Deleted place.' });
 };
